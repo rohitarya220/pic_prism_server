831556a23324a120ec03cc3de0e36caf867eb7c2
Rohit <rohitarya7478@gmail.com>
2024-09-17 21:42:53 +0530

    cors setting changed

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { readdirSync } = require("fs");
 const { connectDb } = require("./connection");
-const cors = require("cors");
+// const cors = require("cors");
 _ = require("underscore");
 const app = express();
 dotenv.config();
 
 // Middleware
-app.use(cors());
+const cors = require('cors');
+const corsOptions = {
+  origin: 'https://pic-prism-server-gjww.onrender.com',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // Routes
